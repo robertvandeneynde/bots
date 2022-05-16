06e81309a3c77a4df18a781d801b137eb795b9f2
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2022-05-16 03:53:07 +0200

    added /caps to discord

--- discord_test.py
+++ discord_test.py
@@ -1,16 +1,47 @@
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+import logging
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        if message.author != client.user:
-            if message.content.lower().startswith("hello"):
-                await message.channel.send("Hello ! :3")
+# logging.basicConfig(level=logging.INFO)
 
-client = MyClient()
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.command()
+async def caps(ctx, *params):
+    text_caps = str(list(params)).upper()
+    await ctx.send(text_caps)
+
+#class MyClient(discord.Client):
+#    async def on_ready(self):
+#        print('Logged on as {0}!'.format(self.user))
+#
+#    async def on_message(self, message):
+#        print('Message from {0.author}: {0.content}'.format(message))
+#        if message.author != self.user:
+#            if message.content.lower().startswith("hello"):
+#                await message.channel.send("Hello ! :3")
+
+#client = MyClient()
+#client = discord.Client()
+
+#@client.event
+#async def on_ready()
+#   print('Logged on as {0}!'.format(client.user))
+
+@bot.event
+async def on_ready():
+   print('Logged on as {0}!'.format(bot.user))
+
+@bot.event
+async def on_message(message):
+    print('Message from {0.author}: {0.content}'.format(message))
+    if message.author != bot.user:
+        if message.content.lower().startswith("hello"):
+            await message.channel.send("Hello ! :3")
+    await bot.process_commands(message)
 
 from discord_settings_local import TOKEN
-client.run(TOKEN)
+bot.run(TOKEN)
+#client.run(TOKEN)
