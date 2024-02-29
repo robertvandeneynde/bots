db6f036ead01d52e1810de2f75ab213160f8d2b6
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2024-02-29 17:12:29 +0100

    Add exception when ambiguous creation of event in timezone environnement

--- telegram_test.py
+++ telegram_test.py
@@ -683,6 +683,9 @@ async def add_event(update: Update, context: CallbackContext):
     read_chat_settings = make_read_chat_settings(update, context)
     chat_timezones = read_chat_settings("event.timezones")
     
+    if chat_timezones and tz not in chat_timezones:
+        raise UserError(f'Your timezone is not in chat timezones, this can be confusing, change your timezone or add your timezone to the chat timezones.\nYour timezone: {tz}\nChat timezones: {chat_timezones}')
+    
     # 1. Send info in text
     await send('\n'.join(filter(None, [
         f"Event {name!r} saved",
