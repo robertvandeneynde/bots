26f9b5237c8084009d8c673f24bd2f14fdb5107b
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2025-03-30 03:38:41 +0300

    Fix wrong code

--- telegram_test.py
+++ telegram_test.py
@@ -166,7 +166,7 @@ async def list_responder(msg: str, send: AsyncSend, *, update, context):
             list_name, parameters_text = match_multi.groups()
             operation = 'editmulti'
             parameters_lines = parameters_text.splitlines()
-            if any(map("-".startswith, parameters_lines)) and not all(map("-".startswith, parameters_lines)):
+            if any(map(lambda x:x.startswith("-"), parameters_lines)) and not all(map(lambda x:x.startswith("-"), parameters_lines)):
                 raise UserError("Either use dash notation or don't, not a mix")
             parameters_lines = [line[1:] if line.startswith("-") else line for line in parameters_lines]
             parameters = list(map(str.strip, parameters_lines))
